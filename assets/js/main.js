18bf1fd1cc91e0ece90a9756f22c0b9b25a2645b
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-17 15:20:45 +0100

    try to add even listener to each cell

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,46 +5,70 @@
 
 // take play btn from DOM 
 
-const playBtnEl = document.querySelector(".play_btn");
+// const playBtnEl = document.querySelector(".play_btn");
 
 //take container from DOM
 
 const containerEl = document.querySelector(".container")
 //create a const for the max cell's number
 
-const maxCellNumb = 100; 
+const maxCellNumb = 100;
 // addEventListener on play button
 
-playBtnEl.addEventListener( "click", 
+playBtnEl.addEventListener("click",
 
-    function() {
-    //create a for loop to create cells "max cell's number's" times
-        
+    function () {
+        //create a for loop to create cells "max cell's number's" times
+
         for (let i = 1; i <= maxCellNumb; i++) {
             const cellEl = `<div class="cell"></div>`
 
-            //append the i value to the cell 
-            console.log (i);
-            cellEl.innerText = str(i); 
+            //log the i value to the cell 
+            console.log(i);
 
             //append the cell to the containerEL  
 
-            containerEl.innerHTML += cellEl; 
-            
+            containerEl.innerHTML += cellEl;
         }
-        
-        
 
 
+
+
+        
     }
+
 )
-   
+
+
 
 // take all the cells from the DOM
+
+const cells = document.querySelectorAll(".cell");
 // create a for to 
-    // add the click event to each cell 
-        // change the bg color with classList 
-        //console.log the number of the cell
+
+for (let i = 0; i < cells.lenght; i++) {
+  
+    const thisCell = cells[i];
+      // add the click event to each cell 
+
+    thisCell.addEventListener("click",
+
+        function () {
+            thisCell.classList.toggle("bg_lightblue"); 
+           
+
+        }
+    )
+    // change the bg color with classList 
+    //console.log the number of the cell
+}
+
+
+
+
+
+
+
 
 
 
