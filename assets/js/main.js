6df690379123d769c1175488764004c6ca7a89b7
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-17 16:06:47 +0100

    add cell's numbers

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,63 +5,67 @@
 
 // take play btn from DOM 
 
-// const playBtnEl = document.querySelector(".play_btn");
+const playBtnEl = document.querySelector(".play_btn");
 
 //take container from DOM
 
 const containerEl = document.querySelector(".container")
-//create a const for the max cell's number
+//create a const for the cell's max number
 
 const maxCellNumb = 100;
 // addEventListener on play button
 
+
 playBtnEl.addEventListener("click",
 
     function () {
-        //create a for loop to create cells "max cell's number's" times
+        //create a for loop to create cells "cell's max number" times
 
         for (let i = 1; i <= maxCellNumb; i++) {
             const cellEl = `<div class="cell"></div>`
 
-            //log the i value to the cell 
-            console.log(i);
+            // //log the i value to the cell 
+            // console.log(i);
 
             //append the cell to the containerEL  
 
             containerEl.innerHTML += cellEl;
         }
 
+        // take all the cells from the DOM
+
+        const cells = document.querySelectorAll(".cell");
+        // create a for to 
+
+        for (let i = 0; i < cells.length; i++) {
+            
+            const thisCell = cells[i];
+            thisCell.append (i + 1)
+            // add the click event to each cell 
+
+            thisCell.addEventListener("click",
+                // change the bg color with classList 
+               
+                function () {
+                    thisCell.classList.toggle("bg_lightblue");
+                    console.log(i + 1);
+                   
+
+
+                }
+            )
+
+        }
 
 
 
-        
+
     }
 
 )
 
 
 
-// take all the cells from the DOM
-
-const cells = document.querySelectorAll(".cell");
-// create a for to 
-
-for (let i = 0; i < cells.lenght; i++) {
-  
-    const thisCell = cells[i];
-      // add the click event to each cell 
-
-    thisCell.addEventListener("click",
-
-        function () {
-            thisCell.classList.toggle("bg_lightblue"); 
-           
-
-        }
-    )
-    // change the bg color with classList 
-    //console.log the number of the cell
-}
 
 
 
