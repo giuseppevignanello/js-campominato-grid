ef604252d2ce24dfd057f4ba7a514d81febfab6a
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-17 14:35:18 +0100

    problem analysis

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,33 @@
 // Ogni cella ha un numero progressivo, da 1 a 100.
 // Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 // Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+
+// take play btn from DOM 
+//take container from DOM
+//create a const for the max cell's number
+// addEventListener on play button
+    //create a for loop to create cells "max cell's number's" times
+        //appen the i value to the cell 
+        //append the cell to the containerEL 
+
+// take all the cells from the DOM
+// create a for to 
+    // add the click event to each cell 
+        // change the bg color with classList 
+        //console.log the number of the cell
+
+
+
+
+
+
+
+// tools
+// -addEventListener
+//for loop
+// varibles
+// log
+// createElement
+// querySelector/getElementbyadd
+// append
+//classList(toggle)
